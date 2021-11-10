1c841eecfe3e3fe5664dd573b368e895ed3cb4d9
shin.kumagai <shin.kumagai.216@gmail.com>
2021-11-10 10:53:12 +0000

    spell miss

--- team.rb
+++ team.rb
@@ -9,24 +9,24 @@ class Team
     self.draw = draw
   end
   
-  def clac_win_rate
+  def calc_win_rate
         self.win / (self.win + self.lose).to_f
   end
   
-  def show_tam_result
-        puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分､勝率は#{clac_win_rate}です。"
+  def show_team_result
+        puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分､勝率は#{calc_win_rate}です。"
   end
 end
 giants_2020result = Team.new('Giants', 67, 45, 8)
 tigers_2020result = Team.new('Tigers', 60, 53, 7)
 dragons_2020result = Team.new('Dragons', 60, 55, 5)
-baystars_2020result = Team.new('Baystars', 56, 58, 6)
+baystars_2020result = Team.new('BayStars', 56, 58, 6)
 carp_2020result = Team.new('Carp', 52, 56, 12)
 swallows_2020result = Team.new('Swallows', 41, 69, 10)
 
-giants_2020result.show_tam_result
-tigers_2020result.show_tam_result
-dragons_2020result.show_tam_result
-baystars_2020result.show_tam_result
-carp_2020result.show_tam_result
-swallows_2020result.show_tam_result
+giants_2020result.show_team_result
+tigers_2020result.show_team_result
+dragons_2020result.show_team_result
+baystars_2020result.show_team_result
+carp_2020result.show_team_result
+swallows_2020result.show_team_result
