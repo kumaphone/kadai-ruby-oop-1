85f410f384fd17e9d36e107b1bc7e59f96816d84
shin.kumagai <shin.kumagai.216@gmail.com>
2021-11-10 10:40:20 +0000

    add teamname

--- team.rb
+++ team.rb
@@ -17,16 +17,16 @@ class Team
         puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分､勝率は#{clac_win_rate}です。"
   end
 end
-result1 = Team.new('Giants', 67, 45, 8)
-result2 = Team.new('Tigers', 60, 53, 7)
-result3 = Team.new('Dragons', 60, 55, 5)
-result4 = Team.new('Baystars', 56, 58, 6)
-result5 = Team.new('Carp', 52, 56, 12)
-result6 = Team.new('Swallows', 41, 69, 10)
+giants_2020result = Team.new('Giants', 67, 45, 8)
+tigers_2020result = Team.new('Tigers', 60, 53, 7)
+dragons_2020result = Team.new('Dragons', 60, 55, 5)
+baystars_2020result = Team.new('Baystars', 56, 58, 6)
+carp_2020result = Team.new('Carp', 52, 56, 12)
+swallows_2020result = Team.new('Swallows', 41, 69, 10)
 
-result1.show_tam_result
-result2.show_tam_result
-result3.show_tam_result
-result4.show_tam_result
-result5.show_tam_result
-result6.show_tam_result
+giants_2020result.show_tam_result
+tigers_2020result.show_tam_result
+dragons_2020result.show_tam_result
+baystars_2020result.show_tam_result
+carp_2020result.show_tam_result
+swallows_2020result.show_tam_result
